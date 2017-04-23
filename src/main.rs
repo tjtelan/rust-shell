124d2df67876f6f8688d94c0f38d235048c96a3b
T.J. Telan <tjtelan@level11.com>
2017-04-23 12:39:12 -0700

    Added splitting on spaces, and printing a prompt to signal to the user when we can input commands

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::io;
+use std::io::{self,Write};
 
 fn main() {
+  let prompt_char = "%";
   loop {
+    print!("{0} ", prompt_char);
+    io::stdout().flush().unwrap();
+
     let mut command = String::new();
     io::stdin().read_line(&mut command)
       .expect("Failed to read in command");
-    println!("{0}", command);
+    println!("{:?}", command);
+
+    let command_split : Vec<&str> = command.split(' ').collect();
+    println!("{:?}", command_split);
   }
 }
