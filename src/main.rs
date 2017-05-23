851b8df121254e20c4f78f895cee6b1fd4c18d40
T.J. Telan <tjtelan@level11.com>
2017-05-23 14:29:46 -0700

    Representing builtins with an enum, and pattern matching to execute them through regular function calls. Implemented echo, but the rest are still left to write

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{self,Write};
+use std::str::FromStr;
 
 fn print_prompt() {
   let prompt_char = "%";
@@ -33,6 +34,60 @@ fn tokenize_command(c : String) -> Command {
   command
 }
 
+enum Builtin {
+  Echo,
+  History,
+  Cd,
+  Pwd
+}
+
+impl FromStr for Builtin {
+  type Err = ();
+  
+  fn from_str(s : &str) -> Result<Self, Self::Err> {
+    match s {
+      "echo" => Ok(Builtin::Echo),
+      "history" => Ok(Builtin::History),
+      "cd" => Ok(Builtin::Cd),
+      "pwd" => Ok(Builtin::Pwd),
+      _ => Err(()),
+    }
+  }
+}
+
+fn builtin_echo(args : &Vec<String>) -> i32 {
+  println!("{}", args.join(" "));
+  0
+}
+
+fn builtin_history(args : &Vec<String>) -> i32 {
+  println!("Not yet implemented");
+  1
+}
+
+fn builtin_cd(args : &Vec<String>) -> i32 {
+  println!("Not yet implemented");
+  1
+}
+
+fn builtin_pwd(args : &Vec<String>) -> i32 {
+  println!("Not yet implemented");
+  1
+}
+
+fn process_command(c : Command) -> i32 {
+  match Builtin::from_str(&c.keyword) {
+    Ok(Builtin::Echo) => builtin_echo(&c.args),
+    Ok(Builtin::History) => builtin_history(&c.args),
+    Ok(Builtin::Cd) => builtin_cd(&c.args),
+    Ok(Builtin::Pwd) => builtin_pwd(&c.args),
+    _ => {
+        println!("{}: command not found", &c.keyword);
+        1
+    },
+  }
+}
+
 fn main() {
   loop {
     print_prompt();
@@ -42,6 +97,8 @@ fn main() {
     println!("DEBUG: keyword : {:?}", command.keyword );
     println!("DEBUG: args : {:?}", command.args );
     
+    let exit_code = process_command(command);
+    println!("DEBUG: Exit code : {:?}", exit_code );
   }
 }
 
